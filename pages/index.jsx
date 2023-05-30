bbc6800e2378cb6f48d57e3e5f2b95ba6683f8a9
sinano1107 <tftf161107@icloud.com>
2023-05-30 13:46:14 +0900

    prettier

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,28 +1,28 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function Header({ title }) {
-    return <h1>{title || 'Default Title'}</h1>
+    return <h1>{title || "Default Title"}</h1>;
 }
 
 export default function HomePage() {
-    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton']
+    const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
 
-    const [likes, setLikes] = useState(0)
-    
+    const [likes, setLikes] = useState(0);
+
     function handleClick() {
-        setLikes(likes + 1)
+        setLikes(likes + 1);
     }
 
     return (
-    <div>
-        <Header title="Develop. Preview. Ship. 🚀" />
-        <ul>
-            {names.map((name) => (
-            <li key={name}>{name}</li>
-            ))}
-        </ul>
-    
-        <button onClick={handleClick}>Like({likes})</button>
-    </div>
-    )
+        <div>
+            <Header title="Develop. Preview. Ship. 🚀" />
+            <ul>
+                {names.map((name) => (
+                    <li key={name}>{name}</li>
+                ))}
+            </ul>
+
+            <button onClick={handleClick}>Like({likes})</button>
+        </div>
+    );
 }
